1cfd5b275ae99c74e1d078a69958f9b64cd623c7
NghiaHH28 <131744068+HHNghia28@users.noreply.github.com>
2024-04-18 11:01:01 +0700

    Add: Mockup classroom

---
+++ src/mock/classroom.js
@@ -0,0 +1,45 @@
+const classroom = {
+  code: 200,
+  status: true,
+  message: "ok",
+  data: {
+    id: 1,
+    name: "Phòng học A1",
+    description: "Phòng học", // allow null
+  },
+};
+
+const classrooms = {
+  code: 200,
+  status: true,
+  message: "ok",
+  data: [
+    {
+      id: 1,
+      name: "Phòng học A1",
+      description: "Phòng học", // allow null
+    },
+    {
+      id: 2,
+      name: "Phòng học A1",
+      description: "Phòng học", // allow null
+    },
+    {
+      id: 3,
+      name: "Phòng học A1",
+      description: "Phòng học", // allow null
+    },
+    {
+      id: 4,
+      name: "Phòng học A1",
+      description: "Phòng học", // allow null
+    },
+    {
+      id: 5,
+      name: "Phòng học A1",
+      description: "Phòng học", // allow null
+    },
+  ],
+};
+
+export { classroom, classrooms };

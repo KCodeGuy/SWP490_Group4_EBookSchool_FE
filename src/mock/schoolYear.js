d29fe4e19b9c34b180971877048fd55344e04f29
NghiaHH28 <131744068+HHNghia28@users.noreply.github.com>
2024-04-21 16:38:01 +0700

    Add: Mockup school year

---
+++ src/mock/schoolYear.js
@@ -0,0 +1,39 @@
+const schoolYear = {
+  code: 200,
+  status: true,
+  message: "ok",
+  data: [
+    {
+      schoolYear: "2018-2019",
+      fromDate: "2/9/2018",
+      toDate: "31/5/2019",
+    },
+    {
+      schoolYear: "2019-2020",
+      fromDate: "2/9/2018",
+      toDate: "31/5/2019",
+    },
+    {
+      schoolYear: "2020-2021",
+      fromDate: "2/9/2018",
+      toDate: "31/5/2019",
+    },
+    {
+      schoolYear: "2021-2022",
+      fromDate: "2/9/2018",
+      toDate: "31/5/2019",
+    },
+    {
+      schoolYear: "2022-2023",
+      fromDate: "2/9/2018",
+      toDate: "31/5/2019",
+    },
+    {
+      schoolYear: "2023-2024",
+      fromDate: "2/9/2018",
+      toDate: "31/5/2019",
+    },
+  ],
+};
+
+export { schoolYear };
